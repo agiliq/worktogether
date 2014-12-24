73c1c0f1f68f7e1a5a0ba6547be37af20a209c0f
Yogesh Kumar <yk.301189@gmail.com>
2014-12-24 11:57:34 +0530

    Removed print statements

--- teamwork/views.py
+++ teamwork/views.py
@@ -11,17 +11,14 @@ from .models import Team, Work
 
 @receiver(sendgrid_email_received, sender=None)
 def receive(sender, **kwargs):
-    print 'received'
     data = kwargs.get('data')
     team_tuple = Team.objects.get_or_create(email=data['sender_email'])
-    print team_tuple
     person = team_tuple[0]
     if team_tuple[1]:
-        person.name=data['sender_name']
+        person.name = data['sender_name']
         person.save()
     work_tuple = Work.objects.get_or_create(person=person,
                                             date=datetime.now())
-    print work_tuple
     work_done = work_tuple[0]
     work_done.work = work_done.work + '\n' + data['body']
     work_done.save()
