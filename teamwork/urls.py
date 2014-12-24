6e1a354520c9741a2122b22d3236fa3baf85c49c
Yogesh Kumar <yk.301189@gmail.com>
2014-12-24 17:24:23 +0530

    Removed teamwork/urls.py

--- teamwork/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from sendgrid_events.views import sendgrid
-
-
-urlpatterns = patterns('',
-                       url('^sendgrid/',
-                           sendgrid, name="sendgrid"), )
