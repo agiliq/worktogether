850843a8819e2ed517498705e1629f38e314005f
Yogesh Kumar <yk.301189@gmail.com>
2014-12-24 17:29:50 +0530

    Updated admin.py

--- teamwork/admin.py
+++ teamwork/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Team, Work
+from .models import TeamMember, WorkDone
 
 
-admin.site.register(Team)
-admin.site.register(Work)
+admin.site.register(TeamMember)
+admin.site.register(WorkDone)
