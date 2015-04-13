89eab0c3e439a265e30dd8e11232a2a74fd4ca48
Yogesh Kumar <yk.301189@gmail.com>
2015-04-13 17:11:42 +0530

    modified admin.py

--- teamwork/admin.py
+++ teamwork/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import TeamMember, WorkDone
 
 
+class WorkDoneAdmin(admin.ModelAdmin):
+
+    list_display = ['date', 'person']
+
 admin.site.register(TeamMember)
-admin.site.register(WorkDone)
+admin.site.register(WorkDone, WorkDoneAdmin)
