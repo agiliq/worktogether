8d847d7e2b2dc5dafc53f4729fb4820ba7b931b9
Yogesh Kumar <yk.301189@gmail.com>
2015-04-14 19:14:11 +0530

    settings change local > base

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worktogether.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worktogether.base")
 
     from django.core.management import execute_from_command_line
 
--- worktogether/wsgi.py
+++ worktogether/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worktogether.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worktogether.base")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
