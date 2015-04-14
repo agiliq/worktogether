9fa46afff2fb60c78b1deea9317806eda72e7466
Yogesh Kumar <yk.301189@gmail.com>
2015-04-14 17:26:27 +0530

    modified settings and wsgi

--- worktogether/base.py
+++ worktogether/base.py
@@ -77,4 +77,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
