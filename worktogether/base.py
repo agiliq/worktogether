6dba20c53567f9b41cbd9019a9e1d24cb9d5d1ab
Yogesh Kumar <yk.301189@gmail.com>
2015-04-15 12:27:09 +0530

    set debug to False

--- worktogether/base.py
+++ worktogether/base.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
