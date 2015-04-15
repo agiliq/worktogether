f68cead94d8c283e45e2beafe83f0eec308dfcd3
Yogesh Kumar <yk.301189@gmail.com>
2015-04-15 14:52:59 +0530

    static root added to urls

--- worktogether/urls.py
+++ worktogether/urls.py
@@ -9,3 +9,7 @@ urlpatterns = patterns('',
                            include('sendgrid_events.urls')),
                        url(r'^admin/',
                            include(admin.site.urls)), )
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
