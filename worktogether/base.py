21fc0466a31c68da0051cb0fa4d2593ee328ab40
Yogesh Kumar <yk.301189@gmail.com>
2015-04-15 19:29:40 +0530

    changed static root path

--- worktogether/base.py
+++ worktogether/base.py
@@ -78,7 +78,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
