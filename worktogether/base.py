26677f4e3e9d6881aeecf93a9d873f7df1da0e44
Yogesh Kumar <yk.301189@gmail.com>
2015-04-15 11:36:00 +0530

    updated installed_apps

--- worktogether/base.py
+++ worktogether/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'teamwork',
+    'sendgrid_events',
 )
 
 MIDDLEWARE_CLASSES = (
