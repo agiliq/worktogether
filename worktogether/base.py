5ab8ce9fe3181949b1df1cea06176f28314f9dab
Yogesh Kumar <yk.301189@gmail.com>
2015-04-30 13:04:49 +0530

    deleted duplicate app names from base settings

--- worktogether/base.py
+++ worktogether/base.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'teamwork',
-    'sendgrid_events',
 )
 
 MIDDLEWARE_CLASSES = (
