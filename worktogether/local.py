70787519616facbb7eb02aa4bdc0ec84a47ddacf
Yogesh Kumar <yk.301189@gmail.com>
2016-07-28 16:55:03 +0530

    Updated settings to avoid throwing error for env vars

--- worktogether/local.py
+++ worktogether/local.py
@@ -3,13 +3,14 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-DB_NAME = os.environ['DB_NAME']
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
+DATABASE_URL = os.environ.get('DATABASE_URL', 'django.db.backends.postgresql_psycopg2')
+DB_NAME = os.environ.get('DB_NAME', '')
+DB_USER = os.environ.get('DB_USER', '')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': DATABASE_URL,
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
@@ -23,8 +24,8 @@ INSTALLED_APPS += (
     'sendgrid_events',
 )
 
-EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
