2356d9fd6748d14ebadc9ad7eac2b8092536ab7e
Yogesh Kumar <yk.301189@gmail.com>
2016-07-28 16:51:07 +0530

    added templates loader

--- worktogether/base.py
+++ worktogether/base.py
@@ -82,7 +82,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
             'loaders': [
-                # insert your TEMPLATE_LOADERS here
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
             ]
         },
     },
