96864d51aecaf585f2332bea06a84645be532685
Yogesh Kumar <yk.301189@gmail.com>
2016-07-29 12:44:01 +0530

    fixed scheduler syntax

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from teamwork.models import ask_team_members, send_digest
 
 
 scheduler = BackgroundScheduler(timezone=utc)
-scheduler.add_job('cron', ask_team_members, day_of_week='mon-fri', hour=18)
-scheduler.add_job('cron', send_digest(), day_of_week='mon-fri', hour=9)
+scheduler.add_job(ask_team_members, 'cron', day_of_week='mon-fri', hour=18)
+scheduler.add_job(send_digest, 'cron', day_of_week='mon-fri', hour=9)
 
 scheduler.start()
