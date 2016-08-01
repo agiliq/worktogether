32ab2a7159c9d24d45b593a4e1a5bd2dd9e1e4ae
Yogesh Kumar <yk.301189@gmail.com>
2016-08-01 18:50:29 +0530

    added __unicode__ to work tracker text

--- teamwork/models.py
+++ teamwork/models.py
@@ -44,6 +44,9 @@ class WorkTrackerText(models.Model):
 
     text = models.TextField()
 
+    def __unicode__(self):
+        return self.text
+
     def clean(self):
         validate_only_one_instance(self)
 
@@ -80,7 +83,7 @@ def ask_team_members():
         else "Tell us what did you get done today?"
     for each in member_email_list:
         send_mail("What have you done today? {0}".format(today),
-                  "{0}".format(text),
+                  "{0}".format(text.),
                   "hello@worksummarizer.agiliq.com",
                   [each, ])
 
