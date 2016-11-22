022f83bd407b0e05525da3c8d6a8c31d49bd3879
Ashik Sujath <ashiksujath2@gmail.com>
2016-11-22 18:43:40 +0530

    angular add/delete tasks

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('worktogether', ['ngRoute']);
 
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+}]);
+
 // app.config(function($routeProvider) {
 //     $routeProvider
 //     .when('/workdone', {
@@ -19,43 +25,73 @@ app.filter('capitalize', function() {
     };
 });
 
-
-app.controller('WorkController', ['$http', '$q', function($http, $q) {
-    var url = WORKDAYLISTURL+DATE;
-    var teamUrl = TEAMLISTURL;
-    var memberId = parseInt(CURRENT_USER_ID);
-    var self = this;
-    var team = {};
-    self.member = {};
-    self.teamWork = [];
-
-    var getTeam = $http.get(teamUrl).then(function(response) {
-        angular.forEach(response.data, function(ele, idx) {
-            if (team[ele.id])
-                angular.extend(team[ele.id], ele);
-            else
-                team[ele.id] = ele;
-            if (!team[ele.id].tasks) team[ele.id].tasks = [];
-        });
-    });
-
-    var getWorkDay = $http.get(url).then(function(response) {
-        angular.forEach(response.data, function(ele, idx) {
-            var person = ele.person;
-            if (team[person.id])
-                angular.extend(team[person.id], {tasks: ele.task_set});
-            else
-                team[person.id] = {tasks: ele.task_set};
-        });
-    });
-    $q.all([getWorkDay, getTeam]).then(function() {
-        angular.forEach(team, function(val, key) {
-            if (key == memberId) self.member = team[key];
-            else self.teamWork.push(team[key]);
-        });
-    });
-
-    self.addTask = function(_task) {
-        self.member.tasks.push({id: 0, task: _task});
+app.factory('WorkServices', ['$http', function($http) {
+    var work = {};
+    var taskAddUrl = TASK_CREATEURL+DATE;
+    var taskDetailUrl = TASK_DETAILURL;
+    work.addTask = function(_task) {
+        return $http.post(taskAddUrl, {task: _task});
     };
+    work.deleteTask = function(_id) {
+        var url = TASK_DETAILURL.split("/");
+        url.pop();
+        url.push(_id);
+        url = url.join("/");
+        return $http.delete(url);
+    };
+    return work;
 }]);
+
+
+app.controller('WorkController', ['$http', '$q', '$filter', 'WorkServices',
+    function($http, $q, $filter, workServices) {
+        var url = WORKDAY_LISTURL+DATE;
+        var teamUrl = TEAM_LISTURL;
+        var memberId = parseInt(CURRENT_USER_ID);
+        var self = this;
+        var team = {};
+        self.member = {};
+        self.teamWork = [];
+        self.newTask = "";
+
+        var getTeam = $http.get(teamUrl).then(function(response) {
+            angular.forEach(response.data, function(ele, idx) {
+                if (team[ele.id])
+                    angular.extend(team[ele.id], ele);
+                else
+                    team[ele.id] = ele;
+                if (!team[ele.id].tasks) team[ele.id].tasks = [];
+            });
+        });
+
+        var getWorkDay = $http.get(url).then(function(response) {
+            angular.forEach(response.data, function(ele, idx) {
+                var person = ele.person;
+                if (team[person.id])
+                    angular.extend(team[person.id], {tasks: ele.task_set});
+                else
+                    team[person.id] = {tasks: ele.task_set};
+            });
+        });
+        $q.all([getWorkDay, getTeam]).then(function() {
+            angular.forEach(team, function(val, key) {
+                if (key == memberId) self.member = team[key];
+                else self.teamWork.push(team[key]);
+            });
+        });
+
+        self.addTask = function() {
+            if (!self.newTask) return;
+            workServices.addTask(self.newTask).then(function(resp) {
+                self.member.tasks.push(resp.data);
+                self.newTask = '';
+            });
+        };
+
+        self.deleteTask = function(_id) {
+            workServices.deleteTask(_id).then(function(){
+                self.member.tasks = $filter('filter')(self.member.tasks, {id: '!'+_id});
+            });
+        };
+    }
+]);
